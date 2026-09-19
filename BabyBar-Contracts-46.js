b42df0c4df03885b0ab7775628311c18cb2ea928
shahramsadeghi999 <shah@cox.net>
2026-09-18 21:00:03 -0700

    Add files via upload

--- BabyBar-Contracts-46.js
+++ BabyBar-Contracts-46.js
@@ -2,7 +2,7 @@
 // Total Questions: 25
 // Subject: Contracts (Common Law & UCC Article 2) ONLY
 // Standard: Master Tier 5 / First-Year Law Students' Examination (Baby Bar) Benchmark
-const examData46 = [
+const examData = [
     {
         id: 1,
         topic: "Formation / Mailbox Rule & Crossing Communications",
